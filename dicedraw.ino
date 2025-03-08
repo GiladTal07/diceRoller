e60ad9cc26df208f78cc0e8ec8e1cda7a5af3f20
Gilad Tal <122132000+GiladTal07@users.noreply.github.com>
2025-03-08 16:32:47 -0500

    Update dicedraw.ino

--- dicedraw.ino
+++ dicedraw.ino
@@ -1,71 +1,71 @@
 //To draw the dice
 
-void drawd4() {
-  tft.drawTriangle(120, 60, 172, 150, 68, 150, TFT_GREEN);
+void drawd4(float f, float x, float y) {
+  tft.drawTriangle(120*f+x, 60*f+y, 172*f+x, 150*f+y, 68*f+x, 150*f+y, TFT_GREEN);
 }
 
-void drawd6() {
-  tft.drawRect(60, 60, 120, 120, TFT_GREEN);
+void drawd6(float f, float x, float y) {
+  tft.drawRect(60*f+x, 60*f+y, 120*f+x, 120*f+x, TFT_GREEN);
 }
 
-void drawd8() {
-  tft.drawLine(120, 172, 165, 146, TFT_GREEN);
-  tft.drawLine(165, 146, 165, 94, TFT_GREEN);
-  tft.drawLine(165, 94, 120, 68, TFT_GREEN);
-  tft.drawLine(120, 68, 75, 94, TFT_GREEN);
-  tft.drawLine(75, 94, 75, 146, TFT_GREEN);
-  tft.drawLine(75, 146, 120, 172, TFT_GREEN);
-  tft.drawTriangle(75, 146, 165, 146, 120, 68, TFT_GREEN);
+void drawd8(float f, float x, float y) {
+  tft.drawLine(120*f+x, 172*f+y, 165*f+x, 146*f+y, TFT_GREEN);
+  tft.drawLine(165*f+x, 146*f+y, 165*f+x, 94*f+y, TFT_GREEN);
+  tft.drawLine(165*f+x, 94*f+y, 120*f+x, 68*f+y, TFT_GREEN);
+  tft.drawLine(120*f+x, 68*f+y, 75*f+x, 94*f+y, TFT_GREEN);
+  tft.drawLine(75*f+x, 94*f+y, 75*f+x, 146*f+y, TFT_GREEN);
+  tft.drawLine(75*f+x, 146*f+y, 120*f+x, 172*f+y, TFT_GREEN);
+  tft.drawTriangle(75*f+x, 146*f+y, 165*f+x, 146*f+y, 120*f+x, 68*f+y, TFT_GREEN);
 }
 
-void drawd10() {
-  tft.drawLine(120, 203, 120, 158, TFT_GREEN);
-  tft.drawLine(68, 134, 38, 138, TFT_GREEN);
-  tft.drawLine(120, 158, 68, 134, TFT_GREEN);
-  tft.drawLine(38, 138, 120, 203, TFT_GREEN);
-  tft.drawLine(120, 203, 120, 158, TFT_GREEN);
-  tft.drawLine(120, 158, 172, 134, TFT_GREEN);
-  tft.drawLine(172, 134, 201, 138, TFT_GREEN);
-  tft.drawLine(201, 138, 120, 203, TFT_GREEN);
-  tft.drawLine(68, 134, 120, 158, TFT_GREEN);
-  tft.drawLine(120, 158, 172, 134, TFT_GREEN);
-  tft.drawLine(172, 134, 120, 36, TFT_GREEN);
-  tft.drawLine(120, 36, 68, 134, TFT_GREEN);
-  tft.drawLine(38, 138, 40, 104, TFT_GREEN);
-  tft.drawLine(40, 104, 120, 36, TFT_GREEN);
-  tft.drawLine(201, 138, 199, 104, TFT_GREEN);
-  tft.drawLine(199, 104, 120, 36, TFT_GREEN);
+void drawd10(float f, float x, float y) {
+  tft.drawLine(120*f+x, 203*f+y, 120*f+x, 158*f+y, TFT_GREEN);
+  tft.drawLine(68*f+x, 134*f+y, 38*f+x, 138*f+y, TFT_GREEN);
+  tft.drawLine(120*f+x, 158*f+y, 68*f+x, 134*f+y, TFT_GREEN);
+  tft.drawLine(38*f+x, 138*f+y, 120*f+x, 203*f+y, TFT_GREEN);
+  tft.drawLine(120*f+x, 203*f+y, 120*f+x, 158*f+y, TFT_GREEN);
+  tft.drawLine(120*f+x, 158*f+y, 172*f+x, 134*f+y, TFT_GREEN);
+  tft.drawLine(172*f+x, 134*f+y, 201*f+x, 138*f+y, TFT_GREEN);
+  tft.drawLine(201*f+x, 138*f+y, 120*f+x, 203*f+y, TFT_GREEN);
+  tft.drawLine(68*f+x, 134*f+y, 120*f+x, 158*f+y, TFT_GREEN);
+  tft.drawLine(120*f+x, 158*f+y, 172*f+x, 134*f+y, TFT_GREEN);
+  tft.drawLine(172*f+x, 134*f+y, 120*f+x, 36*f+y, TFT_GREEN);
+  tft.drawLine(120*f+x, 36*f+y, 68*f+x, 134*f+y, TFT_GREEN);
+  tft.drawLine(38*f+x, 138*f+y, 40*f+x, 104*f+y, TFT_GREEN);
+  tft.drawLine(40*f+x, 104*f+y, 120*f+x, 36*f+y, TFT_GREEN);
+  tft.drawLine(201*f+x, 138*f+y, 199*f+x, 104*f+y, TFT_GREEN);
+  tft.drawLine(199*f+x, 104*f+y, 120*f+x, 36*f+y, TFT_GREEN);
 }
 
-void drawd12() {
-  tft.drawLine(87, 168, 153, 168, TFT_GREEN);
-  tft.drawLine(153, 168, 173, 106, TFT_GREEN);
-  tft.drawLine(173, 106, 120, 68, TFT_GREEN);
-  tft.drawLine(120, 68, 67, 106, TFT_GREEN);
-  tft.drawLine(67, 106, 87, 168, TFT_GREEN);
-  tft.drawLine(120, 34, 67, 52, TFT_GREEN);
-  tft.drawLine(67, 52, 37, 95, TFT_GREEN);
-  tft.drawLine(37, 95, 37, 147, TFT_GREEN);
-  tft.drawLine(37, 147, 67, 192, TFT_GREEN);
-  tft.drawLine(67, 192, 120, 207, TFT_GREEN);
-  tft.drawLine(120, 207, 167, 192, TFT_GREEN);
-  tft.drawLine(167, 192, 202, 147, TFT_GREEN);
-  tft.drawLine(202, 147, 202, 95, TFT_GREEN);
-  tft.drawLine(202, 95, 167, 52, TFT_GREEN);
-  tft.drawLine(167, 52, 120, 34, TFT_GREEN);
-  tft.drawLine(120, 68, 120, 34, TFT_GREEN);
-  tft.drawLine(173, 106, 202, 95, TFT_GREEN);
-  tft.drawLine(153, 168, 167, 192, TFT_GREEN);
-  tft.drawLine(67, 192, 87, 168, TFT_GREEN);
-  tft.drawLine(67, 106, 37, 95, TFT_GREEN);
+void drawd12(float f, float x, float y) {
+  tft.drawLine(87*f+x, 168*f+y, 153*f+x, 168*f+y, TFT_GREEN);
+  tft.drawLine(153*f+x, 168*f+y, 173*f+x, 106*f+y, TFT_GREEN);
+  tft.drawLine(173*f+x, 106*f+y, 120*f+x, 68*f+y, TFT_GREEN);
+  tft.drawLine(120*f+x, 68*f+y, 67*f+x, 106*f+y, TFT_GREEN);
+  tft.drawLine(67*f+x, 106*f+y, 87*f+x, 168*f+y, TFT_GREEN);
+  tft.drawLine(120*f+x, 34*f+y, 67*f+x, 52*f+y, TFT_GREEN);
+  tft.drawLine(67*f+x, 52*f+y, 37*f+x, 95*f+y, TFT_GREEN);
+  tft.drawLine(37*f+x, 95*f+y, 37*f+x, 147*f+y, TFT_GREEN);
+  tft.drawLine(37*f+x, 147*f+y, 67*f+x, 192*f+y, TFT_GREEN);
+  tft.drawLine(67*f+x, 192*f+y, 120*f+x, 207*f+y, TFT_GREEN);
+  tft.drawLine(120*f+x, 207*f+y, 167*f+x, 192*f+y, TFT_GREEN);
+  tft.drawLine(167*f+x, 192*f+y, 202*f+x, 147*f+y, TFT_GREEN);
+  tft.drawLine(202*f+x, 147*f+y, 202*f+x, 95*f+y, TFT_GREEN);
+  tft.drawLine(202*f+x, 95*f+y, 167*f+x, 52*f+y, TFT_GREEN);
+  tft.drawLine(167*f+x, 52*f+y, 120*f+x, 34*f+y, TFT_GREEN);
+  tft.drawLine(120*f+x, 68*f+y, 120*f+x, 34*f+y, TFT_GREEN);
+  tft.drawLine(173*f+x, 106*f+y, 202*f+x, 95*f+y, TFT_GREEN);
+  tft.drawLine(153*f+x, 168*f+y, 167*f+x, 192*f+y, TFT_GREEN);
+  tft.drawLine(67*f+x, 192*f+y, 87*f+x, 168*f+y, TFT_GREEN);
+  tft.drawLine(67*f+x, 106*f+y, 37*f+x, 95*f+y, TFT_GREEN);
 }
 
-void drawd20() {
-  tft.drawTriangle(83, 98, 120, 164, 157, 98, TFT_GREEN);
-  tft.drawTriangle(120, 206, 194, 164, 120, 167, TFT_GREEN);
-  tft.drawTriangle(120, 206, 46, 164, 120, 167, TFT_GREEN);
-  tft.drawTriangle(83, 98, 46, 76, 120, 34, TFT_GREEN);
-  tft.drawTriangle(83, 98, 46, 76, 46, 164, TFT_GREEN);
-  tft.drawTriangle(157, 98, 194, 76, 120, 34, TFT_GREEN);
-  tft.drawTriangle(157, 98, 194, 76, 194, 164, TFT_GREEN);
+void drawd20(float f, float x, float y) {
+  tft.drawTriangle(83*f+x, 98*f+y, 120*f+x, 164*f+y, 157*f+x, 98*f+y, TFT_GREEN);
+  tft.drawTriangle(120*f+x, 206*f+y, 194*f+x, 164*f+y, 120*f+x, 167*f+y, TFT_GREEN);
+  tft.drawTriangle(120*f+x, 206*f+y, 46*f+x, 164*f+y, 120*f+x, 167*f+y, TFT_GREEN);
+  tft.drawTriangle(83*f+x, 98*f+y, 46*f+x, 76*f+y, 120*f+x, 34*f+y, TFT_GREEN);
+  tft.drawTriangle(83*f+x, 98*f+y, 46*f+x, 76*f+y, 46*f+x, 164*f+y, TFT_GREEN);
+  tft.drawTriangle(157*f+x, 98*f+y, 194*f+x, 76*f+y, 120*f+x, 34*f+y, TFT_GREEN);
+  tft.drawTriangle(157*f+x, 98*f+y, 194*f+x, 76*f+y, 194*f+x, 164*f+y, TFT_GREEN);
 }
